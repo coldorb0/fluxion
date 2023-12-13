20d79376850a0cd093dc68fb75d1183d58a576e2
Rizaldy Christoffel <31089448+fireorb@users.noreply.github.com>
2023-12-13 20:24:12 +0900

    Update fluxion.sh

--- fluxion.sh
+++ fluxion.sh
@@ -1,6 +1,5 @@
 apt update
-apt install build-essential libssl-dev libpcap0.8-dev libdigest-hmac-perl
-apt install build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev ethtool shtool rfkill zlib1g-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils clang isc-dhcp-server php-cgi mdk3 lighttpd hostapd macchanger
+apt install libssl-dev libpcap0.8-dev libdigest-hmac-perl build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev ethtool shtool rfkill zlib1g-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils clang isc-dhcp-server php-cgi mdk3 lighttpd hostapd macchanger
 wget https://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
 tar xzvf cowpatty-4.6.tgz
 make cowpatty-4.6/
