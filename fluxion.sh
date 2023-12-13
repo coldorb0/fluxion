5a82c0d1f1a60d1fdb88183c6cc039eabf4e2ed5
Rizaldy Christoffel <31089448+fireorb@users.noreply.github.com>
2023-12-13 20:29:09 +0900

    Update fluxion.sh

--- fluxion.sh
+++ fluxion.sh
@@ -1,11 +1,18 @@
 apt update
 apt install libssl-dev libpcap0.8-dev libdigest-hmac-perl build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev ethtool shtool rfkill zlib1g-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils clang isc-dhcp-server php-cgi mdk3 lighttpd hostapd macchanger
+
 wget https://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
 tar xzvf cowpatty-4.6.tgz
-make cowpatty-4.6/
-sudo make cowpatty-4.6/
+cd cowpatty-4.6/
+make
+sudo make install 
+
+cd ..
 git clone https://github.com/aircrack-ng/mdk4.git
-make mdk4/
-sudo make mdk4/
+cd mdk4/
+make
+sudo make install
+
 git clone https://github.com/FluxionNetwork/fluxion.git
+cd fluxion
 ./fluxion.sh
